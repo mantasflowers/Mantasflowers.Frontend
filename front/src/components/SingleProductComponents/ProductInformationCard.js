4b086c8f3540751a0988ae02650cd4b862185dc9
Maob5579 <mantas.obolevicius@mif.stud.vu.lt>
2021-04-10 00:38:20 +0300

    Created a ProductInformation card. Needs to be divided into components. Maybe .css styling should be adjusted as well.

--- front/src/components/SingleProductComponents/ProductInformationCard.js
+++ front/src/components/SingleProductComponents/ProductInformationCard.js
@@ -5,32 +5,74 @@ import {
   Button,
   Typography,
   Grid,
-  Box,
-  IconButton,
+  Chip,
+  Fab,
 } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: "#ffffff",
     padding: 25,
-    // height: 600,
-    width: "90%",
+    width: "1020px",
   },
-  productDescription: {
-    fontSize: "20px",
-    color: "red",
-    width: "50%",
-    margin: "0 auto",
-  },
-  text: {
-    color: "#000000",
+  description: {
+    color: "#000",
+    marginTop: theme.spacing(4),
+    lineHeight: 2,
   },
   productImageBox: {
-    margin: "120px auto",
+    margin: "120px auto", // xaltura
     textAlign: "center",
   },
   button: {
-    color: "red",
+    borderRadius: 50,
+    background: "#FFF",
+    color: theme.palette.primary.main,
+    "&:hover": {
+      background: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText, // default - balta
+    },
+  },
+  fab: {
+    backgroundColor: "unset",
+    boxShadow: "unset",
+    "&:hover": {
+      background: "unset",
+    },
+    "&.Mui-disabled": {
+      background: "unset",
+    },
+    color: theme.palette.primary.contrastText,
+  },
+  title: {
+    color: "#000",
+    marginBottom: theme.spacing(2),
+  },
+  subtitle: {
+    color: "#000",
+  },
+  fabWrapper: {
+    backgroundColor: theme.palette.primary.main,
+    width: "20%",
+    borderRadius: 50,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    textAlign: "center",
+    alignItems: "center",
+  },
+  fabIcon: {
+    fontSize: 16,
+  },
+  buttonsWrapper: {
+    justifyContent: "space-between",
+    marginTop: theme.spacing(6),
+  },
+  rightSideWrapper: {
+    borderLeft: "2px solid rgba(0,0,0,0.1)", // xaltura
   },
 }));
 
@@ -38,39 +80,80 @@ function ProductInformationCard(props) {
   const classes = useStyles();
 
   const [clicked, setClicked] = useState(false);
-  const [counter, setCounter] = useState(1);
-
-  console.log("counter ->", counter);
+  const [counter, setCounter] = useState(0);
 
   const handleClick = () => {
     setClicked(!clicked);
+    if (counter === 0) {
+      setCounter((s) => s + 1);
+    }
+  };
+  const chips = [{}, {}, {}, {}];
+
+  const handleIncrement = () => setCounter((s) => s + 1);
+
+  const handleDecrement = () => {
+    if (counter === 1) {
+      setClicked(!clicked);
+      setCounter(0);
+    } else setCounter((s) => s - 1);
   };
 
   return (
     <Paper className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid xs={12} md={6}>
-          <Box className={classes.productImageBox}>
-            <img src={props.flower.image} alt="product" />
-          </Box>
+      <Grid container spacing={3} direction="row">
+        <Grid item className={classes.productImageBox}>
+          <img src={props.flower.image} alt="product" />
         </Grid>
-
-        <Grid xs={12} md={6}>
-          <Box></Box>
-          {/* <Typography className={classes.text}>
+        <Grid xs={12} md={6} item className={classes.rightSideWrapper}>
+          <Typography className={classes.title} variant="h3">
+            Rožė
+          </Typography>
+          <Typography className={classes.subtitle} variant="subtitle1">
+            1 vnt.
+          </Typography>
+          <Typography className={classes.description}>
             {props.flower.description}
-          </Typography> */}
-
-          {clicked ? (
-            <Box>
-              <Button className={classes.button}>minusiukas</Button>
-              <Button className={classes.button}>pliusiukas</Button>
-            </Box>
-          ) : (
-            <Button className={classes.button} onClick={() => handleClick()}>
-              1 mygtukas
-            </Button>
-          )}
+          </Typography>
+          <Grid style={{ marginTop: 30 }} container spacing={1}>
+            {chips.map((chip) => (
+              <Grid item>
+                <Chip label="Geles" className={classes.subtitle} />
+              </Grid>
+            ))}
+          </Grid>
+          <Grid className={classes.buttonsWrapper} container>
+            <Typography variant="h3" className={classes.subtitle}>
+              1.5$
+            </Typography>
+            {clicked ? (
+              <Grid className={classes.fabWrapper}>
+                <Fab
+                  size="small"
+                  onClick={handleDecrement}
+                  className={classes.fab}
+                >
+                  <RemoveIcon className={classes.fabIcon} />
+                </Fab>
+                {counter}
+                <Fab
+                  size="small"
+                  onClick={handleIncrement}
+                  className={classes.fab}
+                >
+                  <AddIcon className={classes.fabIcon} />
+                </Fab>
+              </Grid>
+            ) : (
+              <Button
+                variant="contained"
+                className={classes.button}
+                onClick={() => handleClick()}
+              >
+                <AddShoppingCartIcon />Į krepšelį
+              </Button>
+            )}
+          </Grid>
         </Grid>
       </Grid>
     </Paper>
